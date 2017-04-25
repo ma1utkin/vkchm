1c43f0bdc9aa6914ad9610017135d011cb3d0b87
ma1utkin <ma1utko94@mail.ru>
2017-04-25 18:52:26 +0300

    update test

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("test");
+        int i = 5;
+        int b = 10;
+        int result = i << 2;
+        System.out.println(result);
     }
 }
